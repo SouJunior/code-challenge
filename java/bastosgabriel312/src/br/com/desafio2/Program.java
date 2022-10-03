b9b9193ca9407fde69945cd359cad4798484d7c6
bastosgabriel312 <bastosgabriel312@gmail.com>
2022-10-03 17:25:22 -0300

    Desafio 2 completo

--- java/bastosgabriel312/src/br/com/desafio2/Program.java
+++ java/bastosgabriel312/src/br/com/desafio2/Program.java
@@ -3,7 +3,7 @@ package br.com.desafio2;
 public class Program {
 
 	public static int footballPoints(int wins, int draws, int losses) {
-		int totalPoints = 0;
+		int totalPoints = 0 ;
 		totalPoints += wins * 3;
 		totalPoints += draws;
 		return totalPoints;
