d686701f72e9a0ae085f95a1966c2682085fdb8f
amepebbles <71685601+amepebbles@users.noreply.github.com>
2022-10-04 10:04:33 -0300

    Area of a Rectangle

--- bash/amepebbles/4.sh
+++ bash/amepebbles/4.sh
@@ -1,14 +1,2 @@
-### Area of a Rectangle
-Usage: `$ 4.sh <n> <n>`
-
-Examples:
-```
-$ ./4.sh 3 4
-12
-
-$ ./4.sh 3 -3
--1
-
-$ ./4.sh 3 0
--1
-```
+#!/bin/bash
+[[ $1 -le 0 || $2 -le 0 ]] && echo "-1" || echo $(($1*$2))
