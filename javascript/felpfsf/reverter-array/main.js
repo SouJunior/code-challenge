7826c2ce17ba41df1d8182ff5a7e34ce6f36458a
felpfsf <feferreira.sf@gmail.com>
2022-10-04 13:43:34 -0300

    code: added enter key evt listnr, added validation

--- javascript/felpfsf/reverter-array/main.js
+++ javascript/felpfsf/reverter-array/main.js
@@ -2,7 +2,12 @@ const inputEl = document.querySelector('#inputArray')
 const buttonEl = document.querySelector('#button')
 const resultEl = document.querySelector('#resultado')
 
-buttonEl.addEventListener('click', revertArr)
+buttonEl.addEventListener('click', validateInput)
+inputEl.addEventListener('keypress', e => {
+  if (e.key === 'Enter') {
+    validateInput()
+  }
+})
 
 function revertArr() {
   const regex = /\s*,|;\s*/
@@ -12,10 +17,19 @@ function revertArr() {
 
   console.log(`Arr ${typeof newArr} Rev ${typeof newArrRev}`)
 
-  resultEl.innerHTML = `[${newArr}] < > [${newArrRev}]`
+  resultEl.textContent = `[${newArr}] < > [${newArrRev}]`
   clearInput()
 }
 
 function clearInput() {
   inputEl.value = ''
+}
+
+function validateInput() {
+  if (inputEl.value.length === 0) {
+    return (resultEl.innerHTML = `
+    <span class='error__message'>Campo em branco, digite um valor válido nos campos separados por ',' e/ou ';'</span>
+    `)
+  }
+  revertArr()
 }
