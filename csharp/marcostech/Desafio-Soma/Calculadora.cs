39de6a936e6b9ab5445be134a2272a77f7e3e283
Marcos Vinicius <marcosvinicius.aux@gmail.com>
2022-10-08 18:42:20 -0300

    Desafio de Soma

---
+++ csharp/marcostech/Desafio-Soma/Calculadora.cs
@@ -0,0 +1,12 @@
+namespace marcostech.Calculadora
+{
+    public class Calculadora
+    {        
+        ///<param name="numA"> número inteiro a ser somado </param>
+        ///<param name="numB"> número inteiro a ser somado </param>
+        ///<returns> resultado da soma de dois inteiros </returns>
+        public int Somar(int numA, int numB) {
+            return numA + numB;
+        }
+    }
+}
---
+++ csharp/marcostech/Desafio-Soma/README.md
@@ -0,0 +1,2 @@
+Como Executar
+Para executar o código utilize seu terminal de preferência e rode o comando 'dotnet run' dentro da pasta Desafio-Soma
