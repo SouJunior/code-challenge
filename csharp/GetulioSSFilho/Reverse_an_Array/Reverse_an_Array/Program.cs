daf2c1627aceeec5242ef1ce142b04cdcb7b1696
GetulioSSFilho <getuliosergio@hotmail.com>
2022-10-08 23:44:18 -0300

    Chalange

---
+++ csharp/GetulioSSFilho/Reverse_an_Array/Reverse_an_Array/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Reverse_an_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayReverter();
+        }
+
+        static void ArrayReverter()
+        {
+            Console.WriteLine("Digite os números do Array separado por vírgula:");
+            string Array = Console.ReadLine();
+            Array.Split(',').Reverse().ToList().ForEach(x=> Console.Write($"{x} "));
+            Console.ReadLine();
+        }
+    }
+}
