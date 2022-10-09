3b6c60365edadb80daaa8563dbec1730cc4c3d0d
Diego Faria <diego.rdfaria@gmail.com>
2022-10-09 00:23:39 -0300

    Add desafio Return the Sum of Two Numbers

---
+++ csharp/diegodrf/ReturnTheSumOfTwoNumbers/ReturnTheSumOfTwoNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ReturnTheSumOfTwoNumbers;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var num1 = int.Parse(args[0]);
+        var num2 = int.Parse(args[1]);
+        var total = SumTwoNumbers.Addition(num1, num2);
+        Console.WriteLine(total);
+    }
+}
