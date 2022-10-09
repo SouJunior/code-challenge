3e7796108314af90b322eb9ef0bdd8d14d0d1c16
LuisD <luis.domiciano@hotmail.com>
2022-10-08 21:20:13 -0300

    feat: should return a number of points a football team

---
+++ python/luisdomiciano/football-points/football_points.py
@@ -0,0 +1,6 @@
+def football_points (wins, draws, losses):
+    return (wins*3)+(draws*1)+(losses*0)
+
+print (football_points(3, 4, 2))
+print (football_points(5, 0, 2))
+print (football_points(0, 0, 1))
