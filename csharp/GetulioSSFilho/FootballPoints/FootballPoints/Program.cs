376d3190b44e42952fad7cf63f24398960b2c7b7
GetulioSSFilho <getuliosergio@hotmail.com>
2022-10-09 21:21:26 -0300

    Commit inicial

---
+++ csharp/GetulioSSFilho/FootballPoints/FootballPoints/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballPoints
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite abaixo quantas vitórias o time alcançou:");
+            int win = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite abaixo quantas empates o time obteve:");
+            int draw = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite abaixo quantas derrotas o time sofreu:");
+            int loss = int.Parse(Console.ReadLine());
+
+            Console.ForegroundColor= ConsoleColor.Green;
+            Console.WriteLine($"Esse time conquistou um total de {GetPointsCalculated(win, draw, loss)} pontos");
+            Console.ReadLine();
+        }
+        static int GetPointsCalculated(int win, int draw, int loss)
+        {
+            win = win > 0 ? win * 3 : 0;
+            draw = draw > 0 ? draw * 1 : 0;
+            loss = loss > 0 ? 0 : 0;
+            return win + draw + loss;
+        }
+    }
+}
