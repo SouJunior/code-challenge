b291d112550be35a6a632b0c24daffb0e4bf6221
Yan Brasiliano Silva Penalva <yanpenabr@gmail.com>
2022-10-13 09:15:43 -0300

    feat-1: Finalizando desafio 0 -> football points.

---
+++ php/yanbrasiliano/02-football points/app.php
@@ -0,0 +1,8 @@
+<?php
+
+function footballPoints(int $win, int $draw, int $loss): int
+{
+	return floor($win * 3) + ($draw * 1) + ($loss * 0);
+}
+
+echo $response = (isset($argv[1]) && isset($argv[2]) && isset($argv[3])) ? 'Total points is: ' . footballPoints($argv[1], $argv[2], isset($argv[3])) . PHP_EOL : "Values not set correctly, try again." . PHP_EOL;
