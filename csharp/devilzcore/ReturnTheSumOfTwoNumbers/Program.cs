4dcc1a861d2fc43c9b20a7a2c1d6ea4ae0823a82
devilzcore <aldosilva1@live.com>
2022-10-17 18:38:58 -0300

    Add desafio Return the Sum of Two Numbers

---
+++ csharp/devilzcore/ReturnTheSumOfTwoNumbers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class ReturnTheSumOfTwoNumbers
+{
+  public static void Main()
+  {
+    Sum(-3, -6);
+  }
+  static void Sum(int x, int y)
+  {
+    int result;
+    result = x + y;
+
+    Console.WriteLine(result);
+  }
+}
