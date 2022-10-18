fe7e3694f66a8c91bad4297f8868d6b80c260372
Danylo Silva <72108514+DanyloSilva@users.noreply.github.com>
2022-10-18 11:27:40 -0300

    footbal

---
+++ c/DanyloSilva/Football Points/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int w, d, l,soma;
+   scanf("%i", &w);
+   scanf("%i", &d);
+   scanf("%i", &l);
+
+   soma = (w*3) + d ;
+   
+    printf("%i",soma);
+
+    return 0;
+}
