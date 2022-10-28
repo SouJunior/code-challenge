cf95ea234c59031ee05ecae99262db9550bce3e9
PZZZ <pzzz.silva@gmail.com>
2022-10-28 13:45:04 -0300

    desafios-hacktoberfest-2022

---
+++ java/cpazzz/TheSumOfTwoNumbers.java
@@ -0,0 +1,17 @@
+public class TheSumOfTwoNumbers
+{
+
+    public static void main(String[] args)
+    {
+        int a = -3;
+        int b = -6;
+
+        System.out.println(add(a, b));
+    }
+
+    public static int add(int a, int b)
+    {
+        return a + b;
+    }
+
+}
