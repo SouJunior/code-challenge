af38124fd6f1d7ffdac757663537476e5545f23f
joaovictor1516 <voliveira.joao30@sempreceub.com>
2023-05-11 08:40:45 -0300

    adicionando a funcionalidade de soma

--- javascript/joaovictor1516/desafio_1/main.js
+++ javascript/joaovictor1516/desafio_1/main.js
@@ -1,3 +1,17 @@
 const numero1 = document.querySelector("#numero1"),
 numero2 = document.querySelector("#numero2"),
-resultado = document.querySelector("#resultado");
+resultado = document.querySelector("#resultado"),
+somar = document.querySelector("#somar");
+
+function soma(){
+    resultado.innerHTML = "Resultado: ";
+    const soma = Number(numero1.value) + Number(numero2.value);
+    if(isNaN(soma)){
+        resultado.innerHTML = "Valores inválidos.";
+        return "Os valores inválidos."
+    } else {
+        resultado.innerHTML += soma
+        return soma;
+    }
+}
+somar.addEventListener("click", soma)
